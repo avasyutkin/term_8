85d971f2561c1d2ed1140270b11335cda9f60494
Alexander <43503189+avasyutkin@users.noreply.github.com>
2022-03-20 14:17:31 +0300

    common files

---
+++ general_functions.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import os
+
+
+def create_gif(num_of_pictures, num_task):
+    if not os.path.exists("gifs"):
+        os.mkdir("gifs")
+
+    pictures = []
+    im = Image.open('0.png')
+    for picture in range(num_of_pictures):
+        pictures.append(Image.open(f'{picture}.png'))
+
+    im.save(f'gifs/out_task_{num_task}.gif', save_all=True, append_images=pictures, duration=2000, loop=0)
+
+
+def delete_pictures(num_of_pictures):
+    for picture in range(num_of_pictures):
+        os.remove(f'{picture}.png')
+
+
+def check_num(num):
+    if not num.isnumeric():
+        return False
+
+    elif int(num) < 2:
+        return False
+
+    else:
+        return True
