d9bc6af7f5ad57b7cc749d6ab3a28907d344ae59
Alexander <43503189+avasyutkin@users.noreply.github.com>
2022-04-04 15:11:46 +0300

    unittest task_7 upd

--- unit_tests/test_task_7.py
+++ unit_tests/test_task_7.py
@@ -3,16 +3,23 @@ import tasks.task_7 as task_7
 
 test_data = {
     'edges': '(x1,y5),(x1,y6),(x2,y3),(x2,y4),(x2,y5),(x3,y1),(x3,y2),(x3,y4),(x3,y5),(x4,y2),(x4,y3),(x4,y5),(x5,y2),(x5,y6)',
-    'perfect_matching': [['x1', 'y6'], ['x2', 'y4'], ['x3', 'y1'], ['x4', 'y3'], ['x5', 'y2']]
-  }
+    'perfect_matching': [[['x1', 'y6'], ['x2', 'y4'], ['x3', 'y1'], ['x4', 'y3'], ['x5', 'y2']],
+                         [['x1', 'y5'], ['x2', 'y4'], ['x3', 'y2'], ['x4', 'y3'], ['x5', 'y6']],
+                         [['x1', 'y5'], ['x2', 'y3'], ['x3', 'y1'], ['x4', 'y2'], ['x5', 'y6']],
+                         [['x1', 'y6'], ['x2', 'y4'], ['x3', 'y1'], ['x4', 'y5'], ['x5', 'y2']],
+                         [['x1', 'y5'], ['x2', 'y4'], ['x3', 'y1'], ['x4', 'y2'], ['x5', 'y6']],
+                         [['x1', 'y6'], ['x2', 'y3'], ['x3', 'y4'], ['x4', 'y5'], ['x5', 'y2']],
+                         [['x1', 'y5'], ['x2', 'y4'], ['x3', 'y1'], ['x4', 'y3'], ['x5', 'y6']],
+                         [['x1', 'y6'], ['x2', 'y3'], ['x3', 'y1'], ['x4', 'y5'], ['x5', 'y2']],
+                         [['x1', 'y5'], ['x2', 'y3'], ['x3', 'y4'], ['x4', 'y2'], ['x5', 'y6']],
+                         [['x1', 'y6'], ['x2', 'y5'], ['x3', 'y1'], ['x4', 'y3'], ['x5', 'y2']],
+                         [['x1', 'y6'], ['x2', 'y5'], ['x3', 'y4'], ['x4', 'y3'], ['x5', 'y2']],
+                         [['x1', 'y6'], ['x2', 'y4'], ['x3', 'y5'], ['x4', 'y3'], ['x5', 'y2']]]
+}
 
 
 class TestTask5(unittest.TestCase):
     def test_task_5(self):
         graph, pos, edges = task_7.create_graph(test_data['edges'])
         perfect_matching = task_7.find_perfect_matching(graph, edges)
-        self.assertEqual(perfect_matching, test_data['perfect_matching'])
-
-
-
-
+        self.assertIn(perfect_matching, test_data['perfect_matching'])
